1c0bfc90e190cf7ccdf668ff63db691c6e8ac378
Saúl Ruiz <pakrisparkour@gmail.com>
2021-09-12 16:00:27 -0500

    ticket features

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
 const app = express();
 
 app.use(require('./auth/auth.controller'));
 app.use(require('./user/user.controller'));
 app.use(require('./nlp/nlp.controller'));
+app.use(require('./ticket/ticket.controller'));
 
 module.exports = app;
