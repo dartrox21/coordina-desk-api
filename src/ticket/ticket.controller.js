7e2f410a19b4c1baacceb6a3149fbc384a819e64
Saúl Ruiz <pakrisparkour@gmail.com>
2021-09-17 11:38:55 -0500

    adding validation to ticket endpoint to get the roles

--- src/ticket/ticket.controller.js
+++ src/ticket/ticket.controller.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
-const { asyncWrapper } = require('../utils/util.functions');
+const { asyncWrapper, preAuthorize } = require('../utils/util.functions');
 const TicketService = require('./ticket.service');
 const { cleanModel } = require('../middlewares/util.middlewares');
 const Ticket = require('./Ticket.model');
-const cleanMiddleware = cleanModel(Ticket.schema.paths);
+const ROLE = require('../role/Role.enum');
 
+const cleanMiddleware = cleanModel(Ticket.schema.paths);
 
 
 router.post('/ticket', [cleanMiddleware], asyncWrapper(TicketService.create));
 
-router.get('/ticket/dashboard', asyncWrapper(TicketService.dashboard));
+router.get('/ticket/dashboard', [preAuthorize(ROLE.COORDINATOR, ROLE.ASSISTANT)],asyncWrapper(TicketService.dashboard));
 
 router.get('/ticket/:id', asyncWrapper(TicketService.getById));
 
