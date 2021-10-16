0c83686774695bfea5f16df118e684c894476939
Saúl Ruiz <pakrisparkour@gmail.com>
2021-10-16 12:20:33 -0500

    do not delete a category with dependencies

--- src/category/category.service.js
+++ src/category/category.service.js
@@ -2,6 +2,7 @@ const CustomErrorMessages = require("../exceptionHandler/CustomErrorMessages");
 const CustomValidateException = require("../exceptionHandler/CustomValidateException");
 const GenericService = require("../generics/GenericService");
 const Category = require('./Category.model');
+const faqService = require("./faq/faq.service");
 
 
 class CategoryService extends GenericService {
@@ -19,7 +20,26 @@ class CategoryService extends GenericService {
         }
     }
 
-    // delete delete an element that does not has dependencies
+    /**
+     * Deletes a category if it does not has faqs
+     * @param Request req 
+     * @param Response res 
+     * @returns 200 OK if the category is deleted
+     *          409 conflict if the category has inner faqs
+     */
+    delete = async (req, res) => {
+        console.log('delete CategoryService');
+        const id = req.params.id;
+        await this.findByIdAndValidate(id);
+        const faqs = await faqService.getAllObjects({category: id});
+        if(faqs.length > 0) {
+            throw CustomValidateException.conflict()
+                .setField('Has faqs. Faqs count').setValue(faqs.length)
+                .errorMessage(CustomErrorMessages.DELETE_HAS_DEPENDENCIES).build();
+        }
+        await this.genericRepository.delete(id);
+        return res.status(HttpStatus.OK).send();
+    }
 }
 
 module.exports = new CategoryService();
