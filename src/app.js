3df2b4629213a700bdcb625950fe91cda1f4cb07
Saúl Ruiz <pakrisparkour@gmail.com>
2021-10-19 19:32:22 -0500

    Cors Options

--- src/app.js
+++ src/app.js
@@ -8,15 +8,30 @@ const { validateToken } = require('./auth/auth.middleware');
 const cors = require('cors');
 require('./crons');
 
-
+var corsOptions = {
+    origin: 'https://coordinadesk.netlify.app/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+corsOptions = {
+    origin: function(origin, cb) {
+        if([].indexOf(origin) !== -1) {
+            cb(null, true);
+        } else {
+            cb(new Error('Not allowed by cors'));
+        }
+    },
+    methods: "DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 200
+}
 
 app = express();
+
+app.use(cors(corsOptions));
 
 app.use(bodyparser.urlencoded({extended : false}));
 
 app.use(bodyparser.json());
-
-app.use(cors())
 
 app.use(validateToken);
 
