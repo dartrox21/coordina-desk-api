28fecc85021c4d35853a861d7cceebaa622d1aaf
Saúl Ruiz <pakrisparkour@gmail.com>
2021-10-23 11:12:01 -0500

    cors'

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 require('dotenv').config({path: __dirname + '/configuration/.env'});
+const cors = require('cors');
+
 require('./configuration/mongodb.config');
 const express = require('express');
 const bodyparser = require('body-parser');
 const routes = require('./routes');
 const errorMiddleware = require('./middlewares/error.middleware');
 const { validateToken } = require('./auth/auth.middleware');
+const app = require('./routes');
 require('./crons');
-require('./configuration/cors');
+// require('./configuration/cors');
 
 
 
 app = express();
+
+app.use(cors());
 
 app.use(bodyparser.urlencoded({extended : false}));
 
