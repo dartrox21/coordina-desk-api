d5687ac3e4be7109cb119290c7f875660ac05a0c
Saúl Ruiz <pakrisparkour@gmail.com>
2021-10-23 11:17:30 -0500

    rollback cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 require('dotenv').config({path: __dirname + '/configuration/.env'});
-const cors = require('cors');
-
 require('./configuration/mongodb.config');
 const express = require('express');
 const bodyparser = require('body-parser');
 const routes = require('./routes');
 const errorMiddleware = require('./middlewares/error.middleware');
 const { validateToken } = require('./auth/auth.middleware');
-const app = require('./routes');
+const cors = require('cors');
 require('./crons');
 // require('./configuration/cors');
 
@@ -15,18 +13,17 @@ require('./crons');
 
 app = express();
 
-app.use(cors());
-
 app.use(bodyparser.urlencoded({extended : false}));
 
 app.use(bodyparser.json());
+
+app.use(cors())
 
 app.use(validateToken);
 
 app.use(routes);
 
 app.use(errorMiddleware);
-
 app.listen(process.env.PORT ,() => {
     console.log(`Running in NODE_ENV: ${process.env.NODE_ENV}`);
     console.log(`Running EDP in PORT: ${process.env.PORT}`);
