62f10ed110139a522c12097782e65a2e8afbf56b
Saúl Ruiz <pakrisparkour@gmail.com>
2021-10-23 17:35:07 -0500

    rate limiter

--- src/configuration/rateLimit.js
+++ src/configuration/rateLimit.js
@@ -10,7 +10,6 @@ limiter = (app) => {
     const rateLimiter = rateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 1000, // limit each IP to 1000 requests per windowMs
-        message: 'Too many requests, please try again later.'
     });
     app.use(rateLimiter);
 }
