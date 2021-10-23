c8c158fbefec9bee1190fccf1398ff1333f482b1
Saúl Ruiz <pakrisparkour@gmail.com>
2021-10-23 13:23:50 -0500

    testing cron jobs

--- src/auth/token/token.cron.service.js
+++ src/auth/token/token.cron.service.js
@@ -16,7 +16,7 @@ const TokenService = require('./token.service');
  * 5 days
  */
 // const expiredTokens = cron.schedule('* 15 1 * * *', async () => {
-const expiredTokens = cron.schedule('* 15 14 * * *', async () => {
+const expiredTokens = cron.schedule('* 45 13 * * *', async () => {
     console.log('....::::: CRON JOB EXPIRED TOKENS :::::.....');
     await TokenService.removeAllExpired();
 }, {
