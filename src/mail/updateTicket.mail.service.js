34ce9558475986b7e6fff06ea266b2ae9815d51e
Saúl Ruiz <pakrisparkour@gmail.com>
2021-10-24 13:19:19 -0500

    validating hasEmailUpdates before sending an email

--- src/mail/updateTicket.mail.service.js
+++ src/mail/updateTicket.mail.service.js
@@ -17,13 +17,17 @@ class UpdateTicketService {
     }
 
      async sendMail(ticket) {
-        console.log(`Sending mail to ${ticket.email}`);
-        let body = this.BODY.replace(/USERNAME/g, ticket.name);
-        body = body.replace(/TICKET_ID/g, ticket._id);
-        body = body.replace(/TITLE/g, ticket.title);
-        body = body.replace(/TICKET_STATUS/g, ticket.status);
-        body = body.replace(/PORTAL_URL/g, this.URL);
-        await MailService.sendMail(this.SUBJECT, body, ticket.email);
+        console.log(`Sending update ticket mail to ticket ${ticket._id}`);
+        if(ticket.hasEmailUpdates) {
+            let body = this.BODY.replace(/USERNAME/g, ticket.name);
+            body = body.replace(/TICKET_ID/g, ticket._id);
+            body = body.replace(/TITLE/g, ticket.title);
+            body = body.replace(/TICKET_STATUS/g, ticket.status);
+            body = body.replace(/PORTAL_URL/g, this.URL);
+            await MailService.sendMail(this.SUBJECT, body, ticket.email);
+        } else {
+            console.log(`Not sending update ticket mail to ticket ${ticket._id} due to: Notifications are off`);
+        }
     }
 }
 
