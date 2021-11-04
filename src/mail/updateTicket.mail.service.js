cf4b3474b2d89d0d32fc0ebad5a62ab19c0b60d8
Saúl Ruiz <pakrisparkour@gmail.com>
2021-11-03 22:13:08 -0600

    send first email'

--- src/mail/updateTicket.mail.service.js
+++ src/mail/updateTicket.mail.service.js
@@ -19,17 +19,12 @@ class UpdateTicketService {
 
      async sendMail(ticket) {
         console.log(`Sending update ticket mail to ticket ${ticket._id}`);
-        console.log(getStatusTranslate(ticket.status));
-        if(ticket.hasEmailUpdates) {
-            let body = this.BODY.replace(/USERNAME/g, ticket.name);
-            body = body.replace(/TICKET_ID/g, ticket._id);
-            body = body.replace(/TITLE/g, ticket.title);
-            body = body.replace(/TICKET_STATUS/g, getStatusTranslate(ticket.status));
-            body = body.replace(/PORTAL_URL/g, this.URL);
-            await MailService.sendMail(this.SUBJECT, body, ticket.email);
-        } else {
-            console.log(`Not sending update ticket mail to ticket ${ticket._id} due to: Notifications are off`);
-        }
+        let body = this.BODY.replace(/USERNAME/g, ticket.name);
+        body = body.replace(/TICKET_ID/g, ticket._id);
+        body = body.replace(/TITLE/g, ticket.title);
+        body = body.replace(/TICKET_STATUS/g, getStatusTranslate(ticket.status));
+        body = body.replace(/PORTAL_URL/g, this.URL);
+        await MailService.sendMail(this.SUBJECT, body, ticket.email);
     }
 }
 
